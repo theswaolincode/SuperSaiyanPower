084b6e6f94c61d2b07c28fe2f9378f41a88e8ff2
Daniel Ayala Jabon <imediayala@gmail.com>
2021-03-16 16:32:00 +0100

    proj commit

---
+++ SuperSaiyanPower/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SuperSaiyanPower
+//
+//  Created by Daniel Ayala on 16/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SuperSaiyanPower/SuperSaiyanPowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSaiyanPowerApp.swift
+//  SuperSaiyanPower
+//
+//  Created by Daniel Ayala on 16/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SuperSaiyanPowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
