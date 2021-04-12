93311039664a4e6f3eb17de7c566d5478166f6d5
Daniel Ayala Jabon <imediayala@gmail.com>
2021-04-12 09:17:46 +0200

    minor fix layout sizes

--- SuperSaiyanPower/Modules/ContentView.swift
+++ SuperSaiyanPower/Modules/ContentView.swift
@@ -62,30 +62,6 @@ struct ContentView: View {
     }
 }
 
-struct CircleView: View {
-    let kiPower: String
-    
-    var body: some View {
-        ZStack() {
-            Circle()
-                .trim(from: 0.2, to: 1)
-                .stroke(
-                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
-                    style: StrokeStyle(lineWidth: 10, lineCap: .round)
-                )
-                .frame(width: 150, height: 100)
-                .padding(6)
-            VStack {
-                Text("KI POWER")
-                    .bold()
-                Text("\(kiPower)")
-            }
-            
-        }
-        .padding(6)
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- SuperSaiyanPower/Modules/TableRow.swift
+++ SuperSaiyanPower/Modules/TableRow.swift
@@ -15,13 +15,38 @@ struct TableRow: View {
             Image(uiImage: character.image)
                 .resizable()
                 .scaledToFit()
-            Spacer(minLength: 20)
-            
+                .frame(width: 80, height: 150)
+            Spacer()
             CircleView(kiPower: String(character.kiPower))
+            Spacer()
 
-            Spacer(minLength: 20)
             Text(character.name)
         }.frame(height: 150)
+    }
+}
+
+struct CircleView: View {
+    let kiPower: String
+    
+    var body: some View {
+        ZStack() {
+            Circle()
+                .trim(from: 0.2, to: 1)
+                .stroke(
+                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 10, lineCap: .round)
+                )
+                .frame(width: 90, height: 90)
+                .padding(6)
+            VStack {
+                Text("KI POWER")
+                    .bold()
+                    .font(.caption)
+                Text("\(kiPower)")
+            }
+            
+        }
+        .padding(6)
     }
 }
 
